0ae79de934c912aec1512c90c56ba1462386bd0a
yannych1995 <yannymaly@gmal.com>
2024-09-17 14:16:46 -0500

    Consumiendo API

--- src/main.js
+++ src/main.js
@@ -1,1 +1,57 @@
-console.log('Hola Mundo');
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/',
+    Headers: {
+        'content-Type': 'application/json;charse=utf-8',
+    },
+    params: {
+        'api_key': API_KEY,
+    },
+});
+
+async function getTrendingMoviesPreview(params) {
+    const {data} = await api ('trending/movie/day');
+   const movies = data.results;
+
+    movies.forEach(movie => {
+        
+        const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList'); 
+
+        const movieContainer = document.createElement('div');
+        movieContainer.classList.add('movie-container');
+
+        const movieImg = document.createElement('img')
+        movieImg.classList.add('movie-img');
+        movieImg.setAttribute('alt', movie.title);
+        movieImg.setAttribute('src',
+            'https://image.tmdb.org/t/p/w300/' + movie.backdrop_path 
+         );
+         movieContainer.appendChild(movieImg);
+         trendingPreviewMoviesContainer.appendChild(movieContainer);
+    });
+}
+
+async function getCategoriesPreview(params) {
+    const {data} = await api('genre/movie/list');
+    
+    const categories = data.genres;
+
+    categories.forEach(category => {
+        
+        const trendingPreviewMoviesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list'); 
+
+        const categoryContainer = document.createElement('div');
+        categoryContainer.classList.add('category-container');
+
+        const categoryTitle = document.createElement('h3')
+        categoryTitle.classList.add('category-title');
+        categoryTitle.setAttribute('id', 'id'+ category.id);
+        const categoryTitleText = document.createTextNode(category.name
+        );
+         categoryTitle.appendChild(categoryTitleText);
+         categoryContainer.appendChild(categoryTitle);
+         trendingPreviewMoviesContainer.appendChild(categoryContainer);
+
+    });
+}
+getTrendingMoviesPreview()
+getCategoriesPreview()
